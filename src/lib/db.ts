d2f2e2ecc2316a778ec652e0b06af2e98d3e849e
tech-atom <atommlms@gmail.com>
2026-09-17 16:17:31 +0530

    base

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,12 +3,12 @@ import mysql from "mysql2/promise";
 import { TRAINERS, type Trainer } from "./trainers";
 
 
-  export const dbConfig = {
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+ export const dbConfig = {
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
